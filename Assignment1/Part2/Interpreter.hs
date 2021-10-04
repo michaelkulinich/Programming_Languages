0168443b36395765af95495fb530fb758d4d1e2c
Titan <tmitchell@chapman.edu>
2021-10-03 22:13:29 -0700

    Updated and completed

---
+++ Assignment1/Part2/Interpreter.hs
@@ -0,0 +1,15 @@
+module Interpreter where
+
+import AbsNumbers
+
+eval :: Exp -> Integer
+eval (Num n) = n
+eval (Plus n m) = (eval n) + (eval m)
+eval (Minus n m) = (eval n) - (eval m)
+eval (Times n m) = (eval n) * (eval m)
+
+eval (Divide n m) = (eval n) / (eval m)
+eval (Mod n m) = (eval n) % (eval m)
+eval (Pow n m) = (eval n) ^ (eval m)
+eval (Neg n) = negate (eval n)
+eval (Sqrt n) = floor (sqrt (fromIntegral (eval n)))
