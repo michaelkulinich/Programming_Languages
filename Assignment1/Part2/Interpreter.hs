92c70c6ad294facbe2edcd51f11f4aba7e587777
Titan <tmitchell@chapman.edu>
2021-10-04 21:49:16 -0700

    Update

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -8,7 +8,7 @@ eval (Plus n m) = (eval n) + (eval m)
 eval (Minus n m) = (eval n) - (eval m)
 eval (Times n m) = (eval n) * (eval m)
 
-eval (Divide n m) = (eval n) / (eval m)
+eval (Divide n m) = div (eval n) (eval m)
 eval (Mod n m) = mod (eval n) (eval m)
 eval (Pow n m) = (eval n) ^ (eval m)
 eval (Neg n) = negate (eval n)
