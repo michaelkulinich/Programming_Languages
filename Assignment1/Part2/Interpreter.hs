aa51e23e0066d8adaf9ce9e368866b3709ac09a8
Titan <tmitchell@chapman.edu>
2021-10-04 21:48:18 -0700

    Minor

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -9,7 +9,7 @@ eval (Minus n m) = (eval n) - (eval m)
 eval (Times n m) = (eval n) * (eval m)
 
 eval (Divide n m) = (eval n) / (eval m)
-eval (Mod n m) = (eval n) % (eval m)
+eval (Mod n m) = mod (eval n) (eval m)
 eval (Pow n m) = (eval n) ^ (eval m)
 eval (Neg n) = negate (eval n)
 eval (Sqrt n) = floor (sqrt (fromIntegral (eval n)))
